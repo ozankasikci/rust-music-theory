3e2fa72e048e43b1a16839e8d8992d50dc0cbcac
Ozan Kasikci <kasikciozan@yandex.com>
2020-01-24 17:25:21 +0100

    scale type: remove from_u8 method

--- src/scale/scale_type.rs
+++ src/scale/scale_type.rs
@@ -9,16 +9,6 @@ pub enum ScaleType {
 }
 
 impl ScaleType {
-    pub fn from_u8(val: u8) -> Self {
-        use ScaleType::*;
-        match val {
-            1 => Diatonic,
-            2 => MelodicMinor,
-            3 => HarmonicMinor,
-            _ => Diatonic,
-        }
-    }
-
     pub fn from_mode(mode: &Mode) -> Self {
         use ScaleType::*;
         match mode {
