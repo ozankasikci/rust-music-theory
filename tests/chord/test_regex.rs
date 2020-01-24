0dd8e591494f35a357027e0814f8cc3012c4e54b
Ozan Kasikci <kasikciozan@yandex.com>
2020-01-24 22:08:26 +0100

    tests: chord: add minor tests

--- tests/chord/test_regex.rs
+++ tests/chord/test_regex.rs
@@ -2,6 +2,7 @@ extern crate rust_music_theory as theory;
 use theory::chord::{Chord, Quality::*};
 use theory::note::PitchClass::*;
 
+
 #[cfg(test)]
 mod chord_regex_tests {
     use super::*;
@@ -9,24 +10,41 @@ mod chord_regex_tests {
     #[test]
     fn test_major() {
         let table = vec![
-            ("C Major", C, Major),
-            ("E MAJOR", E, Major),
-            ("C Maj", C, Major),
-            ("Cb MAJ", B, Major),
-            ("C M", C, Major),
-            ("C# Minor", Cs, Minor),
-            ("Ds MINOR", Ds, Minor),
-            ("D MIN", D, Minor),
-            ("C min", C, Minor),
-            ("C m", C, Minor),
-            ("Cm", C, Minor),
-            ("C Augmented", C, Augmented),
-            ("C Augmented", C, Augmented),
+            ("C Major", C),
+            ("E MAJOR", E),
+            ("C Maj", C),
+            ("Cb MAJ", B),
+            ("Cb MAJ Seventh", B),
+            ("C M", C),
+            ("C MaJ Triad", C),
+            ("C Major Seventh", C),
+            ("C M Ninth", C),
         ];
 
-        for (string, pitch, quality) in table {
+        for (string, pitch) in table {
             let chord = Chord::from_regex(string).unwrap();
-            assert_eq!(chord.quality, quality);
+            assert_eq!(chord.quality, Major);
+            assert_eq!(chord.root, pitch);
+        }
+    }
+
+    #[test]
+    fn test_minor() {
+        let table = vec![
+            ("C Minor", C),
+            ("E MINOR", E),
+            ("C Min", C),
+            ("Cb MIN", B),
+            ("Cb MIN Seventh", B),
+            ("C m", C),
+            ("C MiN Triad", C),
+            ("C Minor Seventh", C),
+            ("C m Ninth", C),
+        ];
+
+        for (string, pitch) in table {
+            let chord = Chord::from_regex(string).unwrap();
+            assert_eq!(chord.quality, Minor);
             assert_eq!(chord.root, pitch);
         }
     }
