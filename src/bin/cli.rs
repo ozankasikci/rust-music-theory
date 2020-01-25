c3eee2dec5eeba29647dbbedcceeae2784301a83
Ozan Kasikci <kasikciozan@yandex.com>
2020-01-25 17:31:18 +0100

    bin: cli: add chord subcommand

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,6 +1,7 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use rust_music_theory::scale::Scale;
 use rust_music_theory::note::Notes;
+use rust_music_theory::chord::Chord;
 
 const AVAILABLE_SCALES: [&str; 9] = [
     "Major|Ionian",
@@ -13,6 +14,71 @@ const AVAILABLE_SCALES: [&str; 9] = [
     "HarmonicMinor",
     "MelodicMinor",
 ];
+
+const AVAILABLE_CHORDS: [&str; 22] = [
+    "Major Triad",
+    "Minor Triad",
+    "Suspended2 Triad",
+    "Suspended4 Triad",
+    "Augmented Triad",
+    "Diminished Triad",
+    "Major Seventh",
+    "Minor Seventh",
+    "Augmented Seventh",
+    "Augmented Major Seventh",
+    "Diminished Seventh",
+    "HalfDiminished Seventh",
+    "Minor Major Seventh",
+    "Dominant Seventh",
+    "Dominant Ninth",
+    "Major Ninth",
+    "Dominant Eleventh",
+    "Major Eleventh",
+    "Minor Eleventh",
+    "Dominant Thirteenth",
+    "Major Thirteenth",
+    "Minor Thirteenth",
+];
+
+fn scale_command(scale_matches: &ArgMatches) {
+    match scale_matches.subcommand() {
+        ("list", _) => {
+            println!("Available Scales:");
+            for scale in &AVAILABLE_SCALES {
+                println!(" - {}", scale);
+            }
+        }
+        _ => {}
+    }
+
+    let scale_args = scale_matches.values_of("args")
+        .unwrap()
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let scale = Scale::from_regex(&scale_args).unwrap();
+    scale.print_notes();
+}
+
+fn chord_command(chord_matches: &ArgMatches) {
+    match chord_matches.subcommand() {
+        ("list", _) => {
+            println!("Available chords:");
+            for chord in &AVAILABLE_CHORDS {
+                println!(" - {}", chord);
+            }
+        }
+        _ => {}
+    }
+
+    let chord_args = chord_matches.values_of("args")
+        .unwrap()
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    let chord = Chord::from_regex(&chord_args).unwrap();
+    chord.print_notes();
+}
 
 fn main() {
     let matches = App::new("RustMusicTheory")
@@ -31,27 +97,28 @@ fn main() {
                         .multiple(true)
                 )
         )
+        .subcommand(
+            App::new("chord")
+                .subcommand(
+                    App::new("list")
+                )
+                .arg(
+                    Arg::with_name("args")
+                        .help("chord args")
+                        .required(true)
+                        .multiple(true)
+                )
+        )
         .get_matches();
+
 
     match matches.subcommand() {
         ("scale", Some(scale_matches)) => {
-            match scale_matches.subcommand() {
-                ("list", _) => {
-                    println!("Available Scales:");
-                    for scale in &AVAILABLE_SCALES {
-                       println!(" - {}", scale);
-                    }
-                }
-                _ => {}
-            }
+            scale_command(scale_matches);
+        }
 
-            let scale_args = scale_matches.values_of("args")
-                .unwrap()
-                .collect::<Vec<_>>()
-                .join(" ");
-
-            let scale = Scale::from_regex(&scale_args).unwrap();
-            scale.print_notes();
+        ("chord", Some(chord_matches)) => {
+            chord_command(chord_matches);
         }
         
         _ => unreachable!()
