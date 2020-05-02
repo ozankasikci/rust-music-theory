c1aa3bdb79ba7764bfe793025d2275c0f38ca4cb
Koxiaet <38139193+Koxiaet@users.noreply.github.com>
2020-05-02 15:09:29 +0100

    Use lazy_static

--- src/chord/number.rs
+++ src/chord/number.rs
@@ -1,13 +1,21 @@
 use crate::chord::errors::ChordError;
 use regex::{Match, Regex};
 use strum_macros::Display;
+use lazy_static::lazy_static;
 
-const REGEX_NUMBER_TRIAD: &str = "(?i)(triad)";
-const REGEX_NUMBER_SEVENTH: &str = "(?i)(seventh)";
-const REGEX_NUMBER_MAJOR_SEVENTH: &str = r"(?i)(major\s*seventh)";
-const REGEX_NUMBER_NINTH: &str = "(?i)(ninth)";
-const REGEX_NUMBER_ELEVENTH: &str = "(?i)(eleventh)";
-const REGEX_NUMBER_THIRTEENTH: &str = "(?i)(thirteenth)";
+lazy_static! {
+    static ref NUMBER_REGEXES: Vec<(Regex, Number)> = {
+        use Number::*;
+        vec![
+            (Regex::new("(?i)(triad)").unwrap(), Triad),
+            (Regex::new("(?i)(seventh)").unwrap(), Seventh),
+            (Regex::new(r"(?i)(major\s*seventh)").unwrap(), MajorSeventh),
+            (Regex::new("(?i)(ninth)").unwrap(), Ninth),
+            (Regex::new("(?i)(eleventh)").unwrap(), Eleventh),
+            (Regex::new("(?i)(thirteenth)").unwrap(), Thirteenth),
+        ]
+    };
+}
 
 /// The superscript number after a chord.
 #[derive(Display, Debug, Clone, Copy, PartialEq)]
@@ -23,21 +31,11 @@ pub enum Number {
 impl Number {
     /// Parse the number using a regex.
     pub fn from_regex(string: &str) -> Result<(Self, Option<Match>), ChordError> {
-        use Number::*;
-        let regexes = vec![
-            (Regex::new(REGEX_NUMBER_TRIAD), Triad),
-            (Regex::new(REGEX_NUMBER_SEVENTH), Seventh),
-            (Regex::new(REGEX_NUMBER_MAJOR_SEVENTH), MajorSeventh),
-            (Regex::new(REGEX_NUMBER_NINTH), Ninth),
-            (Regex::new(REGEX_NUMBER_ELEVENTH), Eleventh),
-            (Regex::new(REGEX_NUMBER_THIRTEENTH), Thirteenth),
-        ];
-
-        for (regex, number_enum) in regexes {
-            let mode = regex?.find(string);
+        for (regex, number_enum) in &*NUMBER_REGEXES {
+            let mode = regex.find(string);
 
             if let Some(number_match) = mode {
-                return Ok((number_enum, Some(number_match)));
+                return Ok((*number_enum, Some(number_match)));
             };
         }
 
