65f90bcc388a04b01ccc312e959904acd52b81aa
Henry Sloan <henryksloan@gmail.com>
2020-08-17 15:43:10 -0400

    Consolidate and expand chord tests

--- tests/chord/test_chord.rs
+++ tests/chord/test_chord.rs
@@ -15,36 +15,53 @@ mod chord_tests {
     #[test]
     fn test_all_chords_in_c() {
         let chord_tuples = [
-            (Chord::new(C, Major, Triad), vec![C, E, G]),
-            (Chord::new(C, Minor, Triad), vec![C, Ds, G]),
-            (Chord::new(C, Augmented, Triad), vec![C, E, Gs]),
-            (Chord::new(C, Diminished, Triad), vec![C, Ds, Fs]),
-            (Chord::new(C, Major, Seventh), vec![C, E, G, B]),
-            (Chord::new(C, Minor, Seventh), vec![C, Ds, G, As]),
-            (Chord::new(C, Augmented, Seventh), vec![C, E, Gs, As]),
-            (Chord::new(C, Augmented, MajorSeventh), vec![C, E, Gs, B]),
-            (Chord::new(C, Diminished, Seventh), vec![C, Ds, Fs, A]),
-            (Chord::new(C, HalfDiminished, Seventh), vec![C, Ds, Fs, As]),
-            (Chord::new(C, Minor, MajorSeventh), vec![C, Ds, G, B]),
-            (Chord::new(C, Dominant, Seventh), vec![C, E, G, As]),
+            ((C, Major, Triad), vec![C, E, G]),
+            ((C, Minor, Triad), vec![C, Ds, G]),
+            ((C, Augmented, Triad), vec![C, E, Gs]),
+            ((C, Diminished, Triad), vec![C, Ds, Fs]),
+            ((C, Major, Seventh), vec![C, E, G, B]),
+            ((C, Minor, Seventh), vec![C, Ds, G, As]),
+            ((C, Augmented, Seventh), vec![C, E, Gs, As]),
+            ((C, Augmented, MajorSeventh), vec![C, E, Gs, B]),
+            ((C, Diminished, Seventh), vec![C, Ds, Fs, A]),
+            ((C, HalfDiminished, Seventh), vec![C, Ds, Fs, As]),
+            ((C, Minor, MajorSeventh), vec![C, Ds, G, B]),
+            ((C, Dominant, Seventh), vec![C, E, G, As]),
         ];
 
-        for chord_tuple in chord_tuples.iter() {
-            let (chord, pitches) = chord_tuple;
-            assert_notes(pitches, chord.notes());
+        for (chord, pitches) in chord_tuples.iter() {
+            let classes = &mut pitches.clone();
+            for inversion in 0..pitches.len() {
+                assert_notes(
+                    &classes,
+                    Chord::with_inversion(chord.0, chord.1, chord.2, inversion as u8).notes(),
+                );
+                classes.rotate_left(1);
+            }
         }
     }
 
     #[test]
-    fn test_c_major_triad_inversions() {
-        let mut classes = vec![C, E, G];
-
-        for i in 0..classes.len() {
-            assert_notes(
-                &classes,
-                Chord::with_inversion(C, Major, Triad, i as u8).notes(),
+    fn test_inversion_octaves() {
+        let chord_desc = (G, Major, Ninth);
+        let octaves = [
+            [4u8, 4, 5, 5, 5],
+            [4, 5, 5, 5, 6],
+            [4, 4, 4, 5, 5],
+            [4, 4, 5, 5, 6],
+            [4, 5, 5, 6, 6],
+        ];
+        for inversion in 0..octaves[0].len() {
+            let notes =
+                Chord::with_inversion(chord_desc.0, chord_desc.1, chord_desc.2, inversion as u8)
+                    .notes();
+            assert_eq!(
+                notes
+                    .into_iter()
+                    .map(|note| note.octave)
+                    .collect::<Vec<u8>>(),
+                octaves[inversion]
             );
-            classes.rotate_left(1);
         }
     }
 }
